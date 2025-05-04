a3f8fd40f23b66f0d4f41c4c25aaf2e49a355927
Zac Sweers <pandanomic@gmail.com>
2025-05-04 18:48:03 -0400

    StatusTransformer improvements (#392)

--- compiler/src/main/kotlin/dev/zacsweers/metro/compiler/fir/FirProvidesStatusTransformer.kt
+++ compiler/src/main/kotlin/dev/zacsweers/metro/compiler/fir/FirProvidesStatusTransformer.kt
@@ -8,49 +8,45 @@ import org.jetbrains.kotlin.fir.copyWithNewDefaults
 import org.jetbrains.kotlin.fir.declarations.FirCallableDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirDeclarationStatus
-import org.jetbrains.kotlin.fir.declarations.FirProperty
-import org.jetbrains.kotlin.fir.declarations.FirPropertyAccessor
 import org.jetbrains.kotlin.fir.declarations.FirSimpleFunction
-import org.jetbrains.kotlin.fir.declarations.toAnnotationClassIdSafe
 import org.jetbrains.kotlin.fir.declarations.utils.hasBody
 import org.jetbrains.kotlin.fir.declarations.utils.isOverride
-import org.jetbrains.kotlin.fir.declarations.utils.visibility
 import org.jetbrains.kotlin.fir.expressions.FirAnnotationCall
 import org.jetbrains.kotlin.fir.expressions.FirGetClassCall
+import org.jetbrains.kotlin.fir.extensions.FirDeclarationPredicateRegistrar
 import org.jetbrains.kotlin.fir.extensions.FirStatusTransformerExtension
+import org.jetbrains.kotlin.fir.extensions.predicateBasedProvider
 
 internal class FirProvidesStatusTransformer(session: FirSession) :
   FirStatusTransformerExtension(session) {
+  override fun FirDeclarationPredicateRegistrar.registerPredicates() {
+    register(session.predicates.providesAnnotationPredicate)
+  }
+
   override fun needTransformStatus(declaration: FirDeclaration): Boolean {
+    val isProvides =
+      session.predicateBasedProvider.matches(
+        session.predicates.providesAnnotationPredicate,
+        declaration,
+      )
+
+    if (!isProvides) return false
+
     return when (declaration) {
       is FirCallableDeclaration -> {
-        if (declaration.isOverride) return false
-        if (declaration is FirProperty) return false
-        // Property accessors must match parent property visibility
-        if (declaration is FirPropertyAccessor) {
-          return false
-        }
+        if (declaration.symbol.isOverride) return false
         if (declaration !is FirSimpleFunction) return false
 
         // A later FIR checker will check this case
         if (!declaration.hasBody) return false
 
-        var isProvides = false
         // Can't be applied to annotations with KClass args at the moment
         // https://youtrack.jetbrains.com/issue/KT-76257/
-        var hasKClassArg = false
-        for (annotation in declaration.annotations) {
-          if (annotation.toAnnotationClassIdSafe(session) in session.classIds.providesAnnotations) {
-            isProvides = true
-            continue
+        val hasKClassArg =
+          declaration.annotations.filterIsInstance<FirAnnotationCall>().any { annotation ->
+            annotation.argumentList.arguments.any { it is FirGetClassCall }
           }
-          if (annotation !is FirAnnotationCall) continue
-          if (annotation.argumentList.arguments.any { it is FirGetClassCall }) {
-            hasKClassArg = true
-            break
-          }
-        }
-        isProvides && !hasKClassArg
+        !hasKClassArg
       }
       else -> false
     }
